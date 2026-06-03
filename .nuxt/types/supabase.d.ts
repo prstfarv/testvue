dc256e1671b1ebc9503427bd976bfbbd43f3b454
sshamykaev <sshamykaev@igtel.ru>
2026-06-03 18:22:46 +0500

    Deploy issues solving

--- .nuxt/types/supabase.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#supabase/server' {
-  const serverSupabaseClient: typeof import('C:/me/files/testvue/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseClient
-  const serverSupabaseServiceRole: typeof import('C:/me/files/testvue/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseServiceRole
-  const serverSupabaseUser: typeof import('C:/me/files/testvue/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseUser
-  const serverSupabaseSession: typeof import('C:/me/files/testvue/node_modules/@nuxtjs/supabase/dist/runtime/server/services').serverSupabaseSession
-}
